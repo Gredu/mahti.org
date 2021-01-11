f3ae296839c7b49f2a22bbe08e73322bd275101c
Greatman Lim <greatman.lim@mahti.org>
2021-01-11 15:11:23 +0200

    Refract transition

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -1,37 +1,64 @@
-console.log('Hello from Babel!')
-
 const displays = {
   blogit: false,
   muistiinpanoja: false,
 }
 
-function displayMenu(elementId: string) {
-
-  const cards = document.getElementsByClassName('group-card')
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].classList.add('hidden')
-  }
+function displayMenu(groupId: string) {
 
   const main = document.getElementById('main')
-  const displayElement = document.getElementById(elementId)
+  const groups = document.querySelectorAll('.groups')
 
-  if (displays[elementId]) {
-    const offsetHeight = displayElement.offsetHeight + "px"
-    main.style.transform = `translateY(-${offsetHeight})`
-    displays[elementId] = false
-  } else {
-    
-    for (let i = 0; i < displayElement.children.length; i++) {
-      displayElement.children[i].classList.remove('hidden')
-    }
-
-    const offsetHeight = displayElement.offsetHeight + "px"
-    main.style.transform = `translateY(${offsetHeight})`
-
+  function setDisplaysFalse(): void {
     Object.keys(displays).forEach(k => {
-      displays[k] = displays[k] = false
+      displays[k] = false
     })
-
-    displays[elementId] = true
   }
+
+  function setOpacity(elements: NodeListOf<Element>, opacity: string): void {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.opacity = opacity
+    }
+  }
+
+  function setTransitionY(): void {
+    const displayTarget = Object.keys(displays).filter(k => displays[k] == true)
+    if (displayTarget.length == 1) {
+      const offsetHeight = document.getElementById(displayTarget[0]).offsetHeight + "px"
+      main.style.transform = `translateY(${offsetHeight})`
+    } else {
+      main.style.transform = 'translateY(0)'
+    }
+  }
+
+  function setZindex(): void {
+    document.getElementById(groupId).style.zIndex = "0"
+  }
+
+  function reset(): void {
+    const hideLists = document.querySelectorAll('.group-card')
+    setOpacity(hideLists, "0");
+    for (let i = 0; i < groups.length; i++) {
+      groups[i].style.zIndex = "-1"
+    }
+  }
+
+  function renderDisplay() :void {
+
+    reset()
+    setTransitionY()
+    setZindex()
+
+    const showList = document.querySelectorAll(`#${groupId} .group-card`)
+    setOpacity(showList, "1")
+
+  }
+
+  if (displays[groupId]) {
+    setDisplaysFalse()
+  } else {
+    setDisplaysFalse()
+    displays[groupId] = true
+  }
+
+  renderDisplay()
 }
