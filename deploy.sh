2271bc5baf8c4952366f766e2141ebaec10ca181
Greatman Lim <greatman.lim@mahti.org>
2021-03-17 17:02:02 +0200

    Paste from clipboard

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 git push origin master
 hugo
 pass -c mahti.org/mahti.orgServer
-lftp -c "open -u mahti,$(xclip -o) ftp.mahti.org; set ftp:ssl-allow no; set ssl:verify-certificate no; mirror -R public/ ~/public_html/"
+lftp -c "open -u mahti,$(xclip -sel clip -o) ftp.mahti.org; set ftp:ssl-allow no; set ssl:verify-certificate no; mirror -R public/ ~/public_html/"
 rm -r public
